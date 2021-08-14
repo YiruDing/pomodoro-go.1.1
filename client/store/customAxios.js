92587123fc851ebaa386e53daa02c278370cc14f
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-08-14 18:03:28 -0400

    removed headers

--- client/store/customAxios.js
+++ client/store/customAxios.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import getToken from '../getToken';
 const customAxios = axios.create({
   baseURL: `${process.env.API_URL}/api/`,
-  headers: { authorization: localStorage.getItem('token') },
 });
 
 export default customAxios;
