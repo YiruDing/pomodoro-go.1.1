b1f42481b4dfbdf69f1a08db605fd2a06fa7f107
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-08-14 13:01:18 -0400

    removed middleware

--- server/spotifyconnect/index.js
+++ server/spotifyconnect/index.js
@@ -12,22 +12,20 @@ module.exports = router;
 let client_id = process.env.CLIENT_ID_SPOTIFY;
 let redirect_uri = process.env.API_URL + '/callback';
 
-router.get('/', requireToken, async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
-    if (req.user) {
-      const scope =
-        'user-read-private user-read-email user-read-currently-playing user-read-playback-state user-modify-playback-state user-read-recently-played streaming';
+    const scope =
+      'user-read-private user-read-email user-read-currently-playing user-read-playback-state user-modify-playback-state user-read-recently-played streaming';
 
-      res.redirect(
-        'https://accounts.spotify.com/authorize?' +
-          querystring.stringify({
-            response_type: 'code',
-            client_id: client_id,
-            scope: scope,
-            redirect_uri: redirect_uri,
-          })
-      );
-    } else res.sendStatus(401);
+    res.redirect(
+      'https://accounts.spotify.com/authorize?' +
+        querystring.stringify({
+          response_type: 'code',
+          client_id: client_id,
+          scope: scope,
+          redirect_uri: redirect_uri,
+        })
+    );
   } catch (err) {
     next(err);
   }
