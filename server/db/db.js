629120ba2df8886da837eceed274963523cf7cb8
Yi-Ru Ding <dingyr0925@gmail.com>
2023-09-22 08:44:25 -0400

    230922-2

--- server/db/db.js
+++ server/db/db.js
@@ -26,7 +26,7 @@ if (process.env.DATABASE_URL) {
 //   config
 // );
 const db = new Sequelize(
-    process.env.DATABASE_URL || `postgres://container_name:5432/${databaseName}`,
-    config
-  );
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  config
+);
 module.exports = db;
